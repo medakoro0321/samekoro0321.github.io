6bd942417f4854221043ac3059fbfd070381d0cf
めだころ <153439300+medakoro0321@users.noreply.github.com>
2026-05-06 11:34:08 +0900

    fix: タイピング部分を大幅修正

--- main/index.js
+++ main/index.js
@@ -1,3 +1,4 @@
+// -プランクトン風アニメーション-
 function generatePlankton() {
     const canvas = document.createElement('canvas');
     canvas.style.cssText = `
@@ -68,37 +69,74 @@ function generatePlankton() {
     }
 
     window.addEventListener('resize', resize); // 画面サイズ変更時にリサイズ
-    resize(); // 最初に1回呼んでcanvasサイズと星を初期化
+    resize(); // 最初に1回呼んでcanvasサイズを初期化
     draw();   // アニメーション開始
 }
 
+// -タイピングアニメーション-
+
 let text = "<h2 class=\"title\"> 🦈さめころのホームページへようこそ! </h2>"
 let textDesc = "/* WELCOME TO SAMEKORO's HOMEPAGE. */"
+/*
+ * 海の底に、もう一人の私がいる。
+ * 穏やかで、静かで、重さもなく、やっと自由になれた姿で。
+ * いつかそこへ行けたなら——
+ * 流れがこの真っ黒な世界から、逃してくれるように。
+ */
+let textSubtitle = "Console.WriteLine(\"There\'s a version of me at the bottom of the ocean. \n He looks peaceful — still, weightless, finally free. \n Maybe I\'ll join him someday, hoping the current carries me away from this darkened world.\");"
 const target = document.getElementById("main_title");
 const targetDesc = document.getElementById("main_description");
+const targetSubtitle = document.getElementById("main_subtitle");
 let i = 0;
-let n = 0;
-let isDescription = false;
-let timeout = 60
+let status = 0;
 
 function typing() {
-    if (!isDescription) {
-        if (i < text.length) {
-            target.textContent += text.charAt(i);
-            i++;
-            setTimeout(typing, timeout);
-        } else {
-            isDescription = true;
-            setTimeout(typing, timeout);
-        }
-    } else {
-        if (n < textDesc.length) {
-            targetDesc.textContent += textDesc.charAt(n);
-            n++;
-            setTimeout(typing, timeout);
-        }
+    switch (status) {
+        case 0:
+            // 余白のため+5
+            if (i < text.length + 5) {
+                target.textContent += text.charAt(i);
+                target.style.fontSize = "1em";
+                i++;
+                setTimeout(typing, 60);
+            } else {
+                status++;
+                target.textContent = "🦈さめころのホームページへようこそ!"
+                target.style.fontSize = "1.5em";
+
+                i = 0;
+                setTimeout(typing, 60);
+            }
+            break;
+        case 1:
+            if (i < textDesc.length + 5) {
+                targetDesc.textContent += textDesc.charAt(i);
+                i++;
+                setTimeout(typing, 60);
+            } else {
+                status++;
+                targetDesc.textContent = "WELCOME TO SAMEKORO's HOMEPAGE."
+                targetDesc.style.color = "darkgray"
+                i = 0;
+                setTimeout(typing, 30);
+            }
+            break;
+        case 2:
+            if (i < textSubtitle.length + 5) {
+                const char = textSubtitle.charAt(i);
+                targetSubtitle.innerHTML += char === '\n' ? '<br>' : char;
+                i++;
+                setTimeout(typing, 30);
+            } else {
+                const finallychar = "There\'s a version of me at the bottom of the ocean. \n He looks peaceful — still, weightless, finally free. \n <span id='error'><span style='color: darkgray'>Maybe I\'ll join him someday, hoping the current carries me away from this darkened world.</span></span>"
+                targetSubtitle.innerHTML = finallychar.replaceAll('\n', '<br>');
+            }
+            break;
     }
-    console.log(i,n , isDescription);
+}
+
+function addTag() {
+
 }
 
 typing();
